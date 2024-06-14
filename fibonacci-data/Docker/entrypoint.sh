f0b151b524727f489fc3edca7af39936022482ea
Kaio Rosa <kaiosrosa@gmail.com>
2024-06-14 07:53:52 -0300

    update .env.example +  removing steps

--- fibonacci-data/Docker/entrypoint.sh
+++ fibonacci-data/Docker/entrypoint.sh
@@ -6,14 +6,13 @@ fi
 
 if [ ! -f ".env" ]; then
     echo "Creating env file for env $APP_ENV"
-    cp .env.example .env .env.testing
+    cp .env.example .env
 else
     echo "env file exists."
 fi
 
 
 
-php artisan migrate
 php artisan key:generate
 php artisan cache:clear
 php artisan config:clear
